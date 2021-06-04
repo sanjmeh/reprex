81bad493138116f2075aa43220ae7c181bf002a5
Sanjay <sanjmeh@gmail.com>
2021-06-05 01:02:27 +0530

    cleaned up a bit

--- global.R
+++ global.R
@@ -9,19 +9,11 @@ suppressPackageStartupMessages(library(dygraphs))
 suppressPackageStartupMessages(library(stringr))
 suppressPackageStartupMessages(library(purrr))
 
-cat("\nApplication Starting. Loading data from disk")
-cat("..loaded.")
-
-#===== FUNCTIONS DEFINED HERE =====
-sum2 <- function(x) sum(x,na.rm = T)
-
 
 # NO DATA DYGRAPH
 dy_nodata <- function(title="NO DATA AVAILABLE")  as.ts(x=NA) %>% 
   dygraph(main=title,height = 75) %>% 
   dyOptions(drawGrid = F,drawYAxis = F,drawXAxis = F,titleHeight = 40)
-
-
 
 dygen <- function(tsd=NA,site=NA){
   dychart <- 
@@ -42,11 +34,4 @@ load_parameters <- function(dt){
 }
 
 
-##===== END OF FUNCTIONS=========
-
 cat("\n...DONE with global.R..moving to shiny app")
-
-
-
-
-# pass the json_split mqtt file
